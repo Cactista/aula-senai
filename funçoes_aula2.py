1ccc4ee4ad55a071553f88e1134686769d44ad9a
matheus <matheus.alencar0512@gmail.com>
2026-04-28 15:32:31 -0300

    funçoes

--- funçoes_aula2.py
+++ funçoes_aula2.py
@@ -1,1 +1,10 @@
-print("hello")
+def formatar_real_replace(valor):
+    texto = f"R$ {valor:,.2f}" # padrao do eua R$ 1,234.56
+    texto = texto.replace(",","X")
+    texto = texto.replace(".",",")
+    texto = texto.replace("X",".")
+    return texto
+
+#uso
+preco = 1234.5
+print(formatar_real_replace(preco))  # R$ 1.234,50
