dc87a08d900f485f61c6689d76d65a79354efa19
matheus <matheus.alencar0512@gmail.com>
2026-04-28 16:55:38 -0300

    atualização

---
+++ parametros3.py
@@ -0,0 +1,21 @@
+#criando um dicionario
+def exibir_info(**dados):
+    for chave, valor in dados.items():
+        print(f"{chave}: {valor}")
+
+#criamos um dicionario vazio para armazenar entradas
+
+info_usuario = {}
+
+print("Digite as informações (ou 'sair' na chave para encerrar): ")
+
+while True:
+    chave = input("Nome do campo (ex: profissão)")
+    if chave.lower == 'sair':
+        break
+    valor = input(f"Valor para a {chave}: ")
+    info_usuario[chave] = valor
+
+# Usamos ** para desempacotar o dicionario como argumentos
+
+exibir_info(**info_usuario)
